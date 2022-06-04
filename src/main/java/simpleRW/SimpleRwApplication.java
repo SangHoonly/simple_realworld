3fcda910ccda19d0051abd590f67a8adaeec1158
Blueberry <54464320+lee01097@users.noreply.github.com>
2022-06-04 15:50:07 +0900

    프로젝트 초기화

---
+++ src/main/java/simpleRW/SimpleRwApplication.java
@@ -0,0 +1,13 @@
+package simpleRW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRwApplication.class, args);
+	}
+
+}
